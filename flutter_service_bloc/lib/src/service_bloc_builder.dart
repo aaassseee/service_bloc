69dc4a007e4c7f0730d06749f0c3ffa055fb1481
Jack Liu <ywp033319@gmail.com>
2022-12-30 17:40:17 +0800

    docs: service bloc builder

--- flutter_service_bloc/lib/src/service_bloc_builder.dart
+++ flutter_service_bloc/lib/src/service_bloc_builder.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:service_bloc/service_bloc.dart';
 
 /// [ServiceBlocBuilder] handles building a widget in response to relative state.
 ///
@@ -7,42 +9,58 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 /// when certain parameter is set. For example [onInitial] [onLoading] [onFailed].
 /// [onSuccess] is a required parameter so [buildWhen] will always return true if
 /// state is [ServiceLoadSuccess]
-import 'package:service_bloc/service_bloc.dart';
-
 class ServiceBlocBuilder<
     Bloc extends ServiceBloc<ServiceRequestedEvent, Response>,
     ServiceRequestedEvent extends ServiceRequested,
     Response> extends BlocBuilder<Bloc, ServiceState> {
-  final Widget Function(
-    BuildContext context,
-    ServiceInitial state,
-  )? onInitial;
+  /// A widget builder which is only used when build get build in first time and
+  /// current state is [ServiceInitial]. When [onInitial] is omitted and
+  /// [buildWhen] is omitted, [fallback] widget will be used when first build
+  /// and layout would not update when state is [ServiceInitial]. That means
+  /// layout will be preserved from previous build.
+  final Widget Function(BuildContext context, ServiceInitial state)? onInitial;
 
+  /// A widget builder which is only used when current state is
+  /// [ServiceLoadInProgress]. When [onLoading] is omitted and [buildWhen] is
+  /// omitted, layout would not update when state is [ServiceLoadInProgress].
+  /// That means layout will be preserved from previous build.
   final Widget Function(
-    BuildContext context,
-    ServiceLoadInProgress<ServiceRequestedEvent> state,
-    ServiceRequestedEvent event,
-  )? onLoading;
+      BuildContext context,
+      ServiceLoadInProgress<ServiceRequestedEvent> state,
+      ServiceRequestedEvent event)? onLoading;
 
+  /// A widget builder which is only used when current state is
+  /// [ServiceLoadSuccess].
   final Widget Function(
-    BuildContext context,
-    ServiceLoadSuccess<ServiceRequestedEvent, Response> state,
-    ServiceRequestedEvent event,
-    Response response,
-  ) onSuccess;
+      BuildContext context,
+      ServiceLoadSuccess<ServiceRequestedEvent, Response> state,
+      ServiceRequestedEvent event,
+      Response response) onSuccess;
 
+  /// A widget builder which is only used when current state is
+  /// [ServiceLoadFailure]. When [onFailure] is omitted and [buildWhen] is
+  /// omitted, layout would not update when state is [ServiceLoadFailure]. That
+  /// means layout will be preserved from previous build.
   final Widget Function(
-    BuildContext context,
-    ServiceLoadFailure<ServiceRequestedEvent> state,
-    ServiceRequestedEvent event,
-    dynamic error,
-  )? onFailed;
+      BuildContext context,
+      ServiceLoadFailure<ServiceRequestedEvent> state,
+      ServiceRequestedEvent event,
+      dynamic error)? onFailed;
 
+  /// A fallback widget for [builder] to use when [buildWhen] got passed and
+  /// widget builder function is omitted.
+  ///
+  /// Please make sure, which [RenderObject] is extends by parent widget. You
+  /// should the same type of [RenderObject] with [fallback].
+  /// For example: [RenderBox], [RenderSliver].
   final Widget? fallback;
 
+  /// A constructor for creating a [ServiceBlocBuilder]. Default [buildWhen] is
+  /// used when [buildWhen] is omitted.
   ServiceBlocBuilder({
     Key? key,
     Bloc? bloc,
+    BlocBuilderCondition? buildWhen,
     this.onInitial,
     this.onLoading,
     required this.onSuccess,
@@ -51,26 +69,27 @@ class ServiceBlocBuilder<
   }) : super(
           key: key,
           bloc: bloc,
-          buildWhen: (previous, current) {
-            if (current is ServiceInitial) {
-              return onInitial != null;
-            }
+          buildWhen: buildWhen ??
+              (previous, current) {
+                if (current is ServiceInitial) {
+                  return onInitial != null;
+                }
 
-            if (current is ServiceLoadInProgress<ServiceRequestedEvent>) {
-              return onLoading != null;
-            }
+                if (current is ServiceLoadInProgress<ServiceRequestedEvent>) {
+                  return onLoading != null;
+                }
 
-            if (current
-                is ServiceLoadSuccess<ServiceRequestedEvent, Response>) {
-              return true;
-            }
+                if (current
+                    is ServiceLoadSuccess<ServiceRequestedEvent, Response>) {
+                  return true;
+                }
 
-            if (current is ServiceLoadFailure<ServiceRequestedEvent>) {
-              return onFailed != null;
-            }
+                if (current is ServiceLoadFailure<ServiceRequestedEvent>) {
+                  return onFailed != null;
+                }
 
-            return false;
-          },
+                return false;
+              },
           builder: (context, state) {
             if (state is ServiceInitial) {
               if (onInitial == null) {
